c12457150c7b4b127fc2f817e0e008d8cde5dfb3
Ali-Dawud <ali.dawud202@gmail.com>
2025-07-31 16:52:48 +0300

    Product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute";
+import { seedInitialProducts } from "./service/productService";
+import productModel from "./models/productModel";
+import productRoute from "./routes/productRoute";
 
 const app = express();
 const port = 3001;
@@ -15,7 +18,11 @@ mongoose
     console.log("Failed to connect!", err);
   });
 
+// Seed the Products to Databace
+seedInitialProducts()
+
 app.use("/user", userRoute);
+app.use("/products", productRoute);
 
 app.listen(port, () => {
   console.log(`Server is running at: http://localhost:${port}`);
