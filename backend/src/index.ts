83c0605fae0f5f557dc23c1de76ee0c905ba0f69
Ali-Dawud <ali.dawud202@gmail.com>
2025-08-11 11:34:08 +0300

    All Products in Home page + Integration with API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,15 @@ import { seedInitialProducts } from "./service/productService";
 import productModel from "./models/productModel";
 import productRoute from "./routes/productRoute";
 import cartRoute from "./routes/cartRoute";
+import cors from "cors"
 
 dotenv.config()
 const app = express();
 const port = 3001;
+
 app.use(express.json());
+app.use(cors());
+
 
 mongoose
   .connect(process.env.DATABASE_URL || "")
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,41 @@
+import { Box, Container, Grid } from "@mui/material";
+import ProductCard from "../Components/ProductCard";
+import { useEffect, useState } from "react";
+import type { Product } from "../types/Product";
+import { BASE_URL } from "../constants/baseUrl";
+
 function HomePage() {
-    return(
-        <div>HomePage</div>
-    )
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await fetch(`${BASE_URL}products`);
+        const data = await response.json();
+        setProducts(data);
+      };
+      fetchData();
+    } catch {
+      setError(true);
+    }
+  }, []);
+
+  if (error) {
+    return <Box>Something went wrong, please try again!</Box>;
+  }
+  
+  return (
+    <Container sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        {products.map((p) => (
+          <Grid size={{ xs: 12, md: 4 }}>
+            <ProductCard {...p} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
 
-
-export default HomePage
+export default HomePage;
