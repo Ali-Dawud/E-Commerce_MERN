9ee108e1b033449507e09574dd687f15f54a8225
Ali-Dawud <ali.dawud202@gmail.com>
2025-08-13 11:04:03 +0300

    Cart is Empty

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -21,6 +21,48 @@ function CartPage() {
     clearCart();
   };
 
+   const renderCartItems = () => (
+    <Box display="flex" flexDirection="column" gap={4}>
+      {cartItems.map((item) => (
+        <Box
+          key={item.productId}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{
+            border: 1,
+            borderColor: "#f5f5f5",
+            borderRadius: 5,
+            padding: 1,
+          }}
+        >
+          <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+            <img width={"100px"} height={"100px"} src={item.image} />
+            <Box>
+              <Typography variant="h6">{item.title}</Typography>
+              <Typography>
+                {item.quantity} X {item.unitPrice} USD
+              </Typography>
+              <Button onClick={() => handleDeleteItem(item.productId)} color="error">REMOVE ITEM</Button>
+            </Box>
+          </Box>
+
+          <ButtonGroup variant="contained" aria-label="Basic button group">
+            <Button onClick={() => handleQuantity(item.productId, item.quantity - 1)}>-</Button>
+            <Button onClick={() => handleQuantity(item.productId, item.quantity + 1)}>+</Button>
+          </ButtonGroup>
+        </Box>
+      ))}
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Typography variant="h4">
+          Total Amount: {totalAmount.toFixed(2)} USD
+        </Typography>
+        <Button variant="contained">Go To Checkout</Button>
+      </Box>
+    </Box>
+   )
+
   return (
     <Container fixed sx={{ mt: 2 }}>
       <Box display="flex" flexDirection="row" justifyContent="space-between">
@@ -29,45 +71,11 @@ function CartPage() {
           REMOVE Cart
         </Button>
       </Box>
-      <Box display="flex" flexDirection="column" gap={4}>
-        {cartItems.map((item) => (
-          <Box
-            key={item.productId}
-            display="flex"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            sx={{
-              border: 1,
-              borderColor: "#f5f5f5",
-              borderRadius: 5,
-              padding: 1,
-            }}
-          >
-            <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
-              <img width={"100px"} height={"100px"} src={item.image} />
-              <Box>
-                <Typography variant="h6">{item.title}</Typography>
-                <Typography>
-                  {item.quantity} X {item.unitPrice} USD
-                </Typography>
-                <Button onClick={() => handleDeleteItem(item.productId)} color="error">REMOVE ITEM</Button>
-              </Box>
-            </Box>
-
-            <ButtonGroup variant="contained" aria-label="Basic button group">
-              <Button onClick={() => handleQuantity(item.productId, item.quantity - 1)}>-</Button>
-              <Button onClick={() => handleQuantity(item.productId, item.quantity + 1)}>+</Button>
-            </ButtonGroup>
-          </Box>
-        ))}
-        <Box display="flex" flexDirection="row" justifyContent="space-between">
-          <Typography variant="h4">
-            Total Amount: {totalAmount.toFixed(2)} USD
-          </Typography>
-          <Button variant="contained">Go To Checkout</Button>
-        </Box>
-      </Box>
+      {cartItems.length ? (renderCartItems()) : ( 
+        <Typography>
+          Cart is empty. Please start shopping and add items first.
+        </Typography>
+      )}
     </Container>
   );
 }
